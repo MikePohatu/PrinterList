34cf50da1be3c381fa980e87b0f117cc1c294843
Mike Pohatu <mike@20road.com>
2021-05-08 15:47:16 +1200

    added shortcut

--- Windows/_install.cmd
+++ Windows/_install.cmd
@@ -4,3 +4,5 @@ mkdir "%ProgramFiles%\20Road\PrinterList"
 copy /y "%FILECALLPATH%SetupPrinter.ps1" "%ProgramFiles%\20Road\PrinterList\SetupPrinter.ps1"
 if not exist "%ProgramFiles%\20Road\PrinterList\conf.json" copy /y "%FILECALLPATH%conf.json" "%ProgramFiles%\20Road\PrinterList\conf.json"
 regedit /s "%FILECALLPATH%protocol.reg"
+
+copy /y "%FILECALLPATH%Printer List.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Printer List.lnk"
